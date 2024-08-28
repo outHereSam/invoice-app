5c49e73fe43e7d0a7b1d5c03544a0ee3a5d7a356
Samuel Larbi Danquah <larbisamuel0@gmail.com>
2024-08-28 17:02:05 +0000

    Implements invoice form component changing based on where it's used

--- src/app/components/invoice-form/invoice-form.component.ts
+++ src/app/components/invoice-form/invoice-form.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import {
   FormBuilder,
   FormGroup,
@@ -9,9 +9,15 @@ import {
 } from '@angular/forms';
 import { Store } from '@ngrx/store';
 import { AppState } from '../../state/app.state';
-import { addInvoice } from '../../state/invoices/invoices.actions';
-import { CommonModule } from '@angular/common';
+import {
+  addInvoice,
+  updateInvoice,
+} from '../../state/invoices/invoices.actions';
 import { UniqueIdService } from '../../services/unique-id.service';
+import { CommonModule } from '@angular/common';
+import { Invoice } from '../../interfaces/invoice';
+import { Observable } from 'rxjs';
+import { selectInvoiceById } from '../../state/invoices/invoices.selectors';
 
 @Component({
   selector: 'app-invoice-form',
@@ -22,45 +28,85 @@ import { UniqueIdService } from '../../services/unique-id.service';
 })
 export class InvoiceFormComponent {
   invoiceForm: FormGroup;
+  // items: FormArray;
+  // @Input() formType: 'addInvoice' | 'editInvoice' = 'addInvoice';
+  // isEditInvoice = this.formType === 'editInvoice';
+
+  @Input() invoice: Invoice | null = null;
+
   paymentTermsOptions = [
     { value: 1, label: 'Net 1 Day' },
     { value: 7, label: 'Net 7 Days' },
     { value: 14, label: 'Net 14 Days' },
     { value: 30, label: 'Net 30 Days' },
   ];
-  // items: FormArray;
 
   constructor(
     private fb: FormBuilder,
     private store: Store<AppState>,
-    private generatedIdService: UniqueIdService
+    private generatedId: UniqueIdService
   ) {
     this.invoiceForm = this.fb.group({});
+
     // this.items = this.fb.array([]);
   }
 
   ngOnInit() {
+    this.initForm();
+  }
+
+  initForm() {
     this.invoiceForm = this.fb.group({
-      clientName: ['', Validators.required],
-      clientEmail: ['', [Validators.required, Validators.email]],
-      description: ['', Validators.required],
-      paymentTerms: ['', Validators.required],
+      createdAt: [this.invoice?.createdAt || '', Validators.required],
+      clientName: [this.invoice?.clientName || '', Validators.required],
+      clientEmail: [
+        this.invoice?.clientEmail || '',
+        [Validators.required, Validators.email],
+      ],
+      description: [this.invoice?.description || '', Validators.required],
+      paymentTerms: [this.invoice?.paymentTerms || '', Validators.required],
       senderAddress: this.fb.group({
-        street: ['', Validators.required],
-        city: ['', Validators.required],
-        postCode: ['', Validators.required],
-        country: ['', Validators.required],
+        street: [this.invoice?.senderAddress.street || '', Validators.required],
+        city: [this.invoice?.senderAddress.city || '', Validators.required],
+        postCode: [
+          this.invoice?.senderAddress.postCode || '',
+          Validators.required,
+        ],
+        country: [
+          this.invoice?.senderAddress.country || '',
+          Validators.required,
+        ],
       }),
       clientAddress: this.fb.group({
-        street: ['', Validators.required],
-        city: ['', Validators.required],
-        postCode: ['', Validators.required],
-        country: ['', Validators.required],
+        street: [this.invoice?.clientAddress.street || '', Validators.required],
+        city: [this.invoice?.clientAddress.city || '', Validators.required],
+        postCode: [
+          this.invoice?.clientAddress.postCode || '',
+          Validators.required,
+        ],
+        country: [
+          this.invoice?.clientAddress.country || '',
+          Validators.required,
+        ],
       }),
-      items: this.fb.array([]),
+      items: this.fb.array(
+        this.invoice?.items.map((item) => this.createItemFormGroup(item)) || []
+      ),
     });
 
-    this.addItem();
+    if (!this.invoice) {
+      // don't add a new item by default when edititing
+      this.addItem();
+    }
+  }
+
+  createItemFormGroup(item: any = {}) {
+    return this.fb.group({
+      name: [item.name || '', Validators.required],
+      quantity: [item.quantity || 1, [Validators.required, Validators.min(1)]],
+      price: [item.price || 0, [Validators.required, Validators.min(0)]],
+      total: [{ value: item.total || 0, disabled: true }],
+    });
   }
 
   get items() {
@@ -96,9 +142,10 @@ export class InvoiceFormComponent {
       const createdAt = new Date(formValue.createdAt);
       const paymentDue = new Date(createdAt);
       paymentDue.setDate(paymentDue.getDate() + formValue.paymentTerms);
+
       const newInvoice = {
         ...formValue,
-        id: this.generatedIdService.generateUniqueId(),
+        id: this.generatedId.generateUniqueId(),
         createdAt: createdAt.toISOString().split('T')[0],
         paymentDue: paymentDue.toISOString().split('T')[0],
         status: 'pending' as 'paid' | 'pending' | 'draft',
@@ -111,7 +158,16 @@ export class InvoiceFormComponent {
           0
         ),
       };
-      this.store.dispatch(addInvoice({ invoice: newInvoice }));
+
+      // if the input is not null, we are editing an existing invoice
+      if (this.invoice) {
+        // we are editing
+        this.store.dispatch(
+          updateInvoice({ invoice: { id: this.invoice.id, ...newInvoice } })
+        );
+      } else {
+        this.store.dispatch(addInvoice({ invoice: newInvoice }));
+      }
     }
   }
 }
