17bf64df23810959322435effe011e5bc7b731dd
Samuel Larbi Danquah <larbisamuel0@gmail.com>
2024-08-29 09:58:10 +0000

    Fixes theme switching not working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,10 @@ import { AppPanelComponent } from './components/app-panel/app-panel.component';
 import { Store } from '@ngrx/store';
 import { AppState } from './state/app.state';
 import { loadInvoices } from './state/invoices/invoices.actions';
+import { Observable } from 'rxjs';
+import { ThemeState } from './state/theme/theme.state';
+import { selectThemeMode } from './state/theme/theme.selectors';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -15,14 +19,22 @@ import { loadInvoices } from './state/invoices/invoices.actions';
     HomeComponent,
     InvoiceDetailComponent,
     AppPanelComponent,
+    AsyncPipe,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.sass',
 })
 export class AppComponent {
   title = 'invoice-app';
+  themeMode$: Observable<'light' | 'dark'>;
 
-  constructor(private store: Store<AppState>) {}
+  constructor(private store: Store<AppState>) {
+    this.themeMode$ = this.store.select(selectThemeMode);
+    this.themeMode$.subscribe((themeMode) => {
+      document.body.classList.remove('light-mode', 'dark-mode');
+      document.body.classList.add(`${themeMode}-mode`);
+    });
+  }
 
   ngOnInit() {
     this.store.dispatch(loadInvoices());
