0cd17fc42f2928a3f6048afea84bf0aa29f3a260
Samuel Larbi Danquah <larbisamuel0@gmail.com>
2024-08-29 16:17:44 +0000

    Shows the total number of invoices being shown

--- src/app/components/options/options.component.ts
+++ src/app/components/options/options.component.ts
@@ -3,6 +3,7 @@ import { Store } from '@ngrx/store';
 import { AppState } from '../../state/app.state';
 import { updateFilters } from '../../state/invoices/invoices.actions';
 import {
+  selectFilteredInvoices,
   selectFilters,
   selectTotalInvoices,
 } from '../../state/invoices/invoices.selectors';
@@ -17,11 +18,27 @@ import { AsyncPipe } from '@angular/common';
   styleUrl: './options.component.sass',
 })
 export class OptionsComponent {
-  count: number = 0;
+  // count: number = 0;
+  count = {
+    paid: 0,
+    pending: 0,
+    draft: 0,
+  };
   filters$: Observable<{ paid: boolean; pending: boolean; draft: boolean }>;
   constructor(private store: Store<AppState>) {
-    this.store.select(selectTotalInvoices).subscribe((count) => {
-      this.count = count;
+    // this.store.select(selectTotalInvoices).subscribe((count) => {
+    //   this.count = count;
+    // });
+    this.store.select(selectFilteredInvoices).subscribe((invoices) => {
+      this.count.paid = invoices.filter(
+        (invoice) => invoice.status === 'paid'
+      ).length;
+      this.count.pending = invoices.filter(
+        (invoice) => invoice.status === 'pending'
+      ).length;
+      this.count.draft = invoices.filter(
+        (invoice) => invoice.status === 'draft'
+      ).length;
     });
     this.filters$ = this.store.select(selectFilters);
   }
